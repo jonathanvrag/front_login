d621dfc9270c8e507d18283bda21c280bb61f81d
jonathanvra <jonathan.vra@gmail.com>
2024-05-14 14:52:42 -0500

    feat: Add react-router-dom for routing and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import FormLogin from './components/FormLogin/FormLogin';
+import SuccessfulLogin from './components/SuccessfulLogin/SuccessfulLogin';
 
 function App() {
   return (
     <div className='app'>
-      <FormLogin />
+      <Router>
+        <Routes>
+          <Route path='/successful-login' element={<SuccessfulLogin />} />
+          <Route path='/' element={<FormLogin />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
